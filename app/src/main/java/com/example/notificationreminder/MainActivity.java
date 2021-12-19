98bfc1ba11907baf4fde8f5891a1b41d4c8232aa
mr-woodapple <yeahjasper@gmail.com>
2021-12-19 12:01:47 +0100

    Added a second activity to display entered text

--- app/src/main/java/com/example/notificationreminder/MainActivity.java
+++ app/src/main/java/com/example/notificationreminder/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.notificationreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.NotificationReminder.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    // Method needs to be void, only have View as a parameter and be public to work with onClick()
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
