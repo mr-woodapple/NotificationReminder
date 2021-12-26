3e2aab086c55249a6e99120f2948f8778d1975af
mr-woodapple <yeahjasper@gmail.com>
2021-12-26 12:32:50 +0100

    Custom menu bar for the "info" page added, back button logic added

--- app/src/main/java/com/example/notificationreminder/InfoActivity.java
+++ app/src/main/java/com/example/notificationreminder/InfoActivity.java
@@ -4,9 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.appbar.AppBarLayout;
 
@@ -17,5 +20,17 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        // Set the navigationIcon to redirect a page up
+        View topAppBarSub = findViewById(R.id.topAppBarSub);
+        topAppBarSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
+
+
+
 }
